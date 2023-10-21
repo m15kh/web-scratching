90e1539e7106362a7622d1d0529ebc31be8528bc
Liam <91499974+liam1550@users.noreply.github.com>
2023-10-22 00:26:31 +0330

    Update main.py
    web scratching of hackers news site

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+from os import system as s
+s("cls")
 import requests
+import re
+from bs4 import BeautifulSoup
 
 
-url  = 'https://www.tensorflow.org/'
+url = 'https://news.ycombinator.com/news'
+articles = []
+r = requests.get(url)
+content = BeautifulSoup(r.text, 'html.parser')
 
-response  =requests.get(url)
-print(response)
-.
+for item in content.find_all('tr', class_='athing'):
+	item_a = item.find('span', class_='titleline')
+	item_link = item_a.find('a')['href'] if item_a else None
+	item_text = item_a.get_text(strip=True) if item_a else None
+	next_row = item.find_next_sibling('tr')
+	item_score = next_row.find('span', class_='score')
+	item_score = item_score.get_text(strip=True) if item_score else '0 points'
+	item_comments = next_row.find('a', text=re.compile('\d+(&nbsp;|\s)comment(s?)'))
+	item_comments = item_comments.get_text(strip=True).replace('\xa0', ' ') if item_comments else '0 comments'
+
+	articles.append({
+		'link': item_link,
+		'title': item_text,
+        'score': item_score,
+        'comments': item_comments
+
+	})
+
+for article in articles:
+	print(article)
+	print('-' * 50)
